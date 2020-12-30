289d2b3d0783e27d97af942921349113385edcf3
abrmr <iwcw4i@gmail.com>
2020-12-30 12:44:03 +0200

    update price field in product table, add featured field in product table

--- src/models/ProductProvider.js
+++ src/models/ProductProvider.js
@@ -1,7 +1,4 @@
 module.exports = (sequelize, DataTypes) => sequelize.define('product_providers', {
-  double: {
-    type: DataTypes.DOUBLE,
-  },
   available: {
     type: DataTypes.BOOLEAN,
   },
--- src/models/product.js
+++ src/models/product.js
@@ -11,4 +11,11 @@ module.exports = (sequelize, DataTypes) => sequelize.define('product', {
   image_uri: {
     type: DataTypes.STRING(255),
   },
+  price: {
+    type: DataTypes.DOUBLE,
+  },
+  featured: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: 0,
+  },
 });
