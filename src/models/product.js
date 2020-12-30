d61a11b61b51e82f985b08cd54d40b4f590efb42
abrmr <iwcw4i@gmail.com>
2020-12-30 02:55:21 +0200

    add price to product model

--- src/models/product.js
+++ src/models/product.js
@@ -8,6 +8,9 @@ module.exports = (sequelize, DataTypes) => sequelize.define('product', {
   name: {
     type: DataTypes.STRING(45),
   },
+  price: {
+    type: DataTypes.DECIMAL(20, 2),
+  },
   image_uri: {
     type: DataTypes.STRING(255),
   },
