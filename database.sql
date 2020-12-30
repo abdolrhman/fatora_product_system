042949f5f24024152d4348bef9c13a6df0e5ad2b
abrmr <iwcw4i@gmail.com>
2020-12-30 02:49:50 +0200

    Add the db script

---
+++ database.sql
@@ -0,0 +1,61 @@
+create table categories
+(
+	id int auto_increment
+		primary key,
+	name varchar(45) null,
+	createdAt datetime not null,
+	updatedAt datetime not null,
+	parent_id int null,
+	constraint categories_ibfk_1
+		foreign key (parent_id) references categories (id)
+			on update cascade on delete set null
+);
+
+create index parent_id
+	on categories (parent_id);
+
+create table products
+(
+	id int auto_increment
+		primary key,
+	name varchar(45) null,
+	price decimal(20,2) null,
+	image_uri varchar(255) null,
+	createdAt datetime not null,
+	updatedAt datetime not null,
+	categoryId int null,
+	constraint products_ibfk_1
+		foreign key (categoryId) references categories (id)
+			on update cascade on delete set null
+);
+
+create index categoryId
+	on products (categoryId);
+
+create table providers
+(
+	id int auto_increment
+		primary key,
+	name varchar(45) null,
+	createdAt datetime not null,
+	updatedAt datetime not null
+);
+
+create table product_providers
+(
+	createdAt datetime not null,
+	updatedAt datetime not null,
+	productId int not null,
+	providerId int not null,
+	primary key (productId, providerId),
+	constraint product_providers_ibfk_1
+		foreign key (productId) references products (id)
+			on update cascade on delete cascade,
+	constraint product_providers_ibfk_2
+		foreign key (providerId) references providers (id)
+			on update cascade on delete cascade
+);
+
+create index providerId
+	on product_providers (providerId);
+
--- src/controllers/user/product.controller.js
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
-import axios from 'axios';
-// import { product: Product } from '../../models';
-import { successResponse, errorResponse, uniqueId } from '../../helpers';
-
